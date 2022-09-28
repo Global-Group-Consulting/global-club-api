90c803180c996ee710e1bc6b6212019b5fe5c595
leicaflorian <florian.leica@gmail.com>
2022-09-28 15:12:39 +0200

    Updates homepage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title></title>
+
+  <style>
+      .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+          font-family: Arial, Helvetica, sans-serif;
+      }
+  </style>
+</head>
+<body>
+<div class="container">
+  <h2>"{{$phrase}}"</h2>
+  <h3><em>{{$meaning}}</em></h3>
+</div>
+</body>
+</html>
